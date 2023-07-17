655af9a24da8e691fa1dc1f23ec0e7c31b190545
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-17 18:25:39 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -19,35 +19,10 @@ export default async function decorate(block) {
    const footer = document.createElement('div');
     footer.innerHTML = html;
 
-        //Loop used to set same className to div
-        [...document.querySelectorAll("div")].forEach((element) => {
-            element.className = "col-lg-2 col-md-3 col-sm-4 col-xs-12";
-            });
-
-              //Adding className to first Div
-        document.querySelector("div").offsetParent.lastChild.ownerDocument.all[13].classList = 'container pt50 pb50';
-
-            //Adding className to secondDiv
-        document.querySelector("div").offsetParent.lastChild.ownerDocument.all[14].classList = 'row';
-
-            //Loop used to set same className to h3
-        [...document.querySelectorAll("h3")].forEach((element) => {
-                element.className = "ftr-head mt-xs-20";
-                });
-                
-            //Loop used to set same className to ul
-        [...document.querySelectorAll("ul")].forEach((element) => {
-            element.className = "list-unstyled footer-txt";
-            });
-
-            //Adding className to lastLoop ul tag in connect with us innerHtml div
-        document.querySelector("ul").offsetParent.lastChild.ownerDocument.all[73].classList = 'list-inline footer-txt';
-
     decorateIcons(footer);
     block.append(footer); 
 
-      //Adding id to footer
-         /*  document.querySelector("footer").id += 'footer'; */
+       
 
   }
 }
