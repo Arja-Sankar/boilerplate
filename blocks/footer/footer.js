462dd0b5a649df2b47716275fea4f67d039ee2a4
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-19 11:49:31 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,26 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
-
-    /**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
-export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  //block.textContent = '';
-
-  // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
-
-  if (resp.ok) {
-   const html = await resp.text();
-
-    // decorate footer DOM
- const footer = document.createElement('div');
- footer.innerHTML = html;
-
-  
     //Targetting to Get Footer class
 var targetElem = document.querySelector('.footer');
     //Creating First div
@@ -36,56 +14,4 @@ firstContainer.appendChild(secondContainer);
     //appending created div's to main targeted Element Footer
 targetElem.appendChild(firstContainer,targetElem);
       targetElem.append(targetElem);
-    block.append(targetElem);
-        decorateIcons(footer);
-   block.append(footer);
-    //latest updated Footer js code
-           if(document.body.childNodes.item('').nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.className == 'footer block'){
-            //adding className to first Div in footer
-            document.querySelector("div > div").className += 'container pt50 pb50'; 
-
-             //adding className to second Div in footer
-            document.querySelector("div > div > div").className += 'row';
-
-            //creating div to append Loop (h3,ul) tags starts here ---
-            
-            for(var i=0; i< document.getElementsByTagName("H3").length;i++){
-                const elem = document.createElement('div');
-                const target = document.getElementsByTagName("H3")[i];
-                const elemul = document.getElementsByTagName("ul")[i];
-          
-            // grab target element reference
-           // const target = document.querySelector('H3')[i]
-          // const elemul = document.querySelector('ul')
-
-            // insert the element before target element
-            target.parentNode.insertBefore(elem, target);
-            elem.append(target,elemul);
-
-            //Loop used to set same className to that tag conatins(h3,ul)
-            [...document.querySelectorAll("div > div > div > div")].forEach((element) => {
-                element.className = "col-lg-2 col-md-3 col-sm-4 col-xs-12";
-                });
-            }
-
-            //Loop used to set same className to h3
-            [...document.querySelectorAll("h3")].forEach((element) => {
-                element.className = "ftr-head mt-xs-20";
-                });
-                
-            //Loop used to set same className to ul
-            [...document.querySelectorAll("ul")].forEach((element) => {
-            element.className = "list-unstyled footer-txt";
-            });
-
-            //updating className to lastchild of ul containing in last div with innerHTML as Connect With Us
-            document.body.children.item('footer-wrapper').lastElementChild.parentElement.firstElementChild.children.item("").firstChild.nextElementSibling.lastElementChild.lastChild.className = 'list-inline footer-txt';
-                
-            //Loop ends here ---list-inline footer-txt
-
-                
-        }
-
-
-  }
-}
+ 
