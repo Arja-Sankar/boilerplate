26f3eba61fc9fec0c857ab486b0f09999a2bd970
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-19 11:46:16 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,7 +1,26 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 
-    
+    /**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  const cfg = readBlockConfig(block);
+  //block.textContent = '';
+
+  // fetch footer content
+  const footerPath = cfg.footer || '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+
+  if (resp.ok) {
+   const html = await resp.text();
+
+    // decorate footer DOM
+ const footer = document.createElement('div');
+ footer.innerHTML = html;
+
+  
     //Targetting to Get Footer class
 var targetElem = document.querySelector('.footer');
     //Creating First div
@@ -16,7 +35,10 @@ secondContainer.classList.add('row');
 firstContainer.appendChild(secondContainer);
     //appending created div's to main targeted Element Footer
 targetElem.appendChild(firstContainer,targetElem);
-    
+      targetElem.append(targetElem);
+    block.append(targetElem);
+        decorateIcons(footer);
+   block.append(footer);
     //latest updated Footer js code
            if(document.body.childNodes.item('').nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.className == 'footer block'){
             //adding className to first Div in footer
