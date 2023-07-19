6910ca36835cc48ac79e33239ab153043bcd7d82
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-19 11:29:57 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -22,6 +22,21 @@ export default async function decorate(block) {
    decorateIcons(footer);
    block.append(footer); 
     
+    //Targetting to Get Footer class
+var targetElem = document.querySelector('.footer');
+    //Creating First div
+var firstContainer = document.createElement('div');
+    //creating second div
+var secondContainer = document.createElement('div');
+    //appending className to first div
+firstContainer.classList.add('container','pt50','pb50');
+    //appending className to second div
+secondContainer.classList.add('row');
+    //appending child node to first div
+firstContainer.appendChild(secondContainer);
+    //appending created div's to main targeted Element Footer
+targetElem.appendChild(firstContainer,targetElem);
+    
     //latest updated Footer js code
            if(document.body.childNodes.item('').nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.className == 'footer block'){
             //adding className to first Div in footer
