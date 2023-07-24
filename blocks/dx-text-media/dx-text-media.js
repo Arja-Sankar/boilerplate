805df5688088cb9062e00aee8253db073f217078
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-24 20:54:37 +0530

    Update dx-text-media.js

--- blocks/dx-text-media/dx-text-media.js
+++ blocks/dx-text-media/dx-text-media.js
@@ -8,25 +8,7 @@ const newElement = document.createElement('div');
 newElement.className = 'successstories aem-GridColumn aem-GridColumn--default--12';
 
 
-const successStoriesContainer = newElement.querySelector('.dx-text-media');
-export default function decorate(block) {
-  [...block.children].forEach((story) => {
-  const column = document.createElement('div');
-  column.className = 'col-md-3 col-sm-6 col-xs-12 mb20 relative wow fadeInLeft animated';
-  column.dataset.wowDelay = '0.3s';
-  column.style.visibility = 'visible';
-  column.style.webkitAnimationDelay = '0.3s';
-  column.style.mozAnimationDelay = '0.3s';
-  column.style.animationDelay = '0.3s';
+//const successStoriesContainer = newElement.querySelector('.dx-text-media');
 
-  const imageSrc = story.querySelector('img').getAttribute('src');
-  const imageAlt = story.querySelector('img').getAttribute('alt');
-  const description = story.querySelector('div').textContent;
-
-
-
-  successStoriesContainer.appendChild(column);    
-  });
-}
 // Replace the target element with the new element
 targetElement.parentNode.replaceChild(newElement, targetElement);
