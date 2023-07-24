ba49186ea2355bcc55732f218b1bca56606a88a9
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-24 20:39:37 +0530

    Update dx-text-media.js

--- blocks/dx-text-media/dx-text-media.js
+++ blocks/dx-text-media/dx-text-media.js
@@ -7,30 +7,8 @@ const targetElement = document.querySelector('.dx-text-media-wrapper');
 const newElement = document.createElement('div');
 newElement.className = 'successstories aem-GridColumn aem-GridColumn--default--12';
 
-// Set the HTML content for the new element
-newElement.innerHTML = `
-  <section id="success_stories">
-    <article class="container">
-      <div class="row">
-        <div class="col-md-9 col-sm-9 col-xs-12 wow fadeInDown animated" data-wow-delay="0.3s" style="visibility: visible;-webkit-animation-delay: 0.3s; -moz-animation-delay: 0.3s; animation-delay: 0.3s;">
-          <h2 class="h2-heading white-color">Success Stories</h2>
-          <div class="yellow-dots-left"> <span></span> <span></span> <span></span> </div>
-          <p class="para-txt white-color mb50">Delivering the Complete Customer Experience</p>
-        </div>
-      </div>
-      <div class="row equal-heights">
-      </div>
-      <div class="col-md-12 col-sm-12 col-xs-12 mt50 wow fadeInUp animated" data-wow-delay="1.2s" style="visibility: visible;-webkit-animation-delay: 1.2s; -moz-animation-delay: 1.2s; animation-delay: 1.2s;"> <a class="btn btn-shutter-more casestudy-btn" href="/services/digital-experience/case-studies.html" aria-label="Show More about Success Stories">Show More</a> </div>
-    </article>
-  </section>
-`;
 
-// Get the container element for success stories
-const successStoriesContainer = newElement.querySelector('.equal-heights');
-
-// Get the data from [1] HTML
-const successStories = targetElement.querySelectorAll('.dx-text-media');
-
+const successStoriesContainer = newElement.querySelector('.dx-text-media');
 export default function decorate(block) {
   [...block.children].forEach((story) => {
   const column = document.createElement('div');
@@ -45,24 +23,7 @@ export default function decorate(block) {
   const imageAlt = story.querySelector('img').getAttribute('alt');
   const description = story.querySelector('div').textContent;
 
-  column.innerHTML = `
-    <div class="equal-bg bg-light-white">
-      <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12 p0">
-          <img src="${imageSrc}" alt="${imageAlt}" class="img-responsive get-image-height">
-          <p class="title-box-yellow minus-top">Client Speak</p>
-        </div>
-        <div class="col-md-12 col-sm-12 col-xs-12 relative-off">
-          <div class="eqheight">
-            <p class="caption-txt"><a title="${imageAlt}" class="black-color" href="/industries/healthcare/pacesetters/digital-growth-enterprise.html#pacesettersvideo" target="_self">${description}</a></p>
-            <ul class="list-inline tag-bg">
-              <li class="lct-txt">  </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+
 
   successStoriesContainer.appendChild(column);    
   });
