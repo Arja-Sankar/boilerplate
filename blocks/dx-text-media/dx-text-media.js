54d9b9f1b7fa743e7906b70779fe1caa582775ea
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-28 15:50:39 +0530

    Update dx-text-media.js

--- blocks/dx-text-media/dx-text-media.js
+++ blocks/dx-text-media/dx-text-media.js
@@ -9,9 +9,9 @@
       //adding classNames to main Div
       mainDiv.classList.add('aem-GridColumn','aem-GridColumn--default--12');
    //setting background banner
-var a = document.querySelectorAll('.freeflowhtml > div > div')[0].querySelector('img').src;
-document.querySelector('.dx-text-media-wrapper').style.backgroundImage = url(a);    
-//mainDiv.style.backgroundImage = document.querySelectorAll('.freeflowhtml > div > div')[0].querySelector('img').src;
+// var a = document.querySelectorAll('.freeflowhtml > div > div')[0].querySelector('img').src;
+// document.querySelector('.dx-text-media-wrapper').style.backgroundImage = url(a);    
+mainDiv.style.backgroundImage = url(document.querySelectorAll('.freeflowhtml > div > div')[0].querySelector('img').src);
 
     //creating section element
     var sectionElem = document.createElement('section');
