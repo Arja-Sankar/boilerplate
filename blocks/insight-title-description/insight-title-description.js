446af017619870f25d7654e9712d820abfae9e04
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-28 15:24:11 +0530

    Update insight-title-description.js

--- blocks/insight-title-description/insight-title-description.js
+++ blocks/insight-title-description/insight-title-description.js
@@ -10,14 +10,14 @@
                         <div class="row">
                             <div class="col-md-12 col-sm-12 col-xs-12 text-center wow fadeInDown animated" data-wow-delay="0.3s" style="visibility: visible;-webkit-animation-delay: 0.3s; -moz-animation-delay: 0.3s; animation-delay: 0.3s;">
                                 <img src="${desc.imgsrc}" class="center-block" alt="Line">
-                                <h2 class="h2-heading">${.descheading}</h2>
+                                <h2 class="h2-heading">${desc.heading}</h2>
                                 <p class="col-md-8 col-md-offset-2 para-txt light-gray mb50">${desc.para}</p>
                             </div>
                         </div>
                     </article>
                 </section>`
     }
-    let blockDATA = targetELEMENT.querySelectorAll('.title-description.block > div > div')
+    let blockDATA = targetELEMENT.querySelectorAll('.insight-title-description.block > div > div')
     desc.imgsrc=blockDATA[0].querySelector('img').src
     desc.heading=blockDATA[1].textContent
     desc.para=blockDATA[2].textContent
