dfa920c36e425df36ef10b33d515e5f6f122f7c4
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-07-31 14:36:03 +0530

    Update insights.js

--- blocks/insights/insights.js
+++ blocks/insights/insights.js
@@ -3,7 +3,32 @@ var mainInsightHeading = document.querySelector('.insights-wrapper>.insights.blo
  //adding classNames to main Div
 mainInsightHeading.setAttribute('class','insights');
 mainInsightHeading.classList.add('aem-GridColumn', 'aem-GridColumn--default--12');
-
+//creating section element
+    var insightSectionElem = document.createElement('section');
+    //adding Id to section
+    insightSectionElem.setAttribute('id','insights');
+    insightSectionElem.setAttribute('class','pb75');
+     mainInsightHeading.appendChild(insightSectionElem);
+   
+    //creating article
+    var insightArticleElem = document.createElement('article');
+    //adding class to article
+    insightArticleElem.setAttribute('class','container');
+    insightSectionElem.appendChild(insightArticleElem);
+   
+    //creating div inside Div Container
+    var insightContainerFirstDiv = document.createElement('div');
+    //adding className to container first Div
+    insightContainerFirstDiv.classList.add('row', 'row-eq-ht', 'wow', 'fadeInUp', 'animated');
+    //adding attribute to container first Div
+    insightContainerFirstDiv.setAttribute('data-wow-delay','0.6s');
+    //Adding Inline CSS to container first Div
+    insightContainerFirstDiv.style.visibility = 'visible';
+    insightContainerFirstDiv.style.webkitAnimationDelay = '0.6s';
+    insightContainerFirstDiv.style.mozAnimationDelay = '0.6s';
+    insightContainerFirstDiv.style.animationDelay = '0.6s';
+    insightArticleElem.appendChild(insightContainerFirstDiv);
+    
 
 
 
