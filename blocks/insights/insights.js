20108fd9c2abf94e7ace93f05914a229d465acb4
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-08-01 12:43:02 +0530

    Update insights.js

--- blocks/insights/insights.js
+++ blocks/insights/insights.js
@@ -29,21 +29,88 @@ mainInsightHeading.classList.add('aem-GridColumn', 'aem-GridColumn--default--12'
     insightContainerFirstDiv.style.animationDelay = '0.6s';
     insightArticleElem.appendChild(insightContainerFirstDiv);
 
-    // let commonDiv = document.createElement('div');
-    //  let commonHeader = document.createElement('h5');
-    //  let commonParagraph = document.createElement('p');
+    
+
   
  for(let i=0;i < document.querySelectorAll('.insights > div').length; i++){
      
      let insightContentDiv = document.createElement('div');
     insightContainerFirstDiv.appendChild(insightContentDiv);
    insightContentDiv.classList.add('col-md-6', 'col-sm-12', 'col-xs-12', 'p0');
+  let blockDataImg = document.querySelectorAll('.insights > div');
+    let blockData = document.querySelectorAll('.insights > div > div');
+    let imgsrc = blockDataImg[i].querySelector('img').src;
+    let heading = blockData[i].textContent;
+    let para = blockData[i].textContent;
+    let hreflink = blockData[i].textContent;
+   let commonAnchor = document.createElement('a'); 
+  let commonDiv = document.createElement('div');
+  let commonMainDiv = document.createElement('div');
+  let commonImg = document.createElement('img');
+    let commonHeader = document.createElement('h5');
+    let commonParagraph = document.createElement('p');
+  let d = document.querySelectorAll('#insights >.container > .row > div')[i];
+var commonImgclass , commonHeader , commonParagraph , commonClassOne , commonClassTwo , commonClassThree;
 
+  commonAnchor.setAttribute('href',hreflink);
+  commonAnchor.setAttribute('target','_blank');
+  commonAnchor.setAttribute('title',para);
+  d.appendChild(commonAnchor);
+ 
+  commonClassOne = '';
+ commonDiv.setAttribute('class',commonClassOne);
+ commonAnchor.appendChild(commonDiv);
+
+  commonClassTwo = '';
+ commonMainDiv.setAttribute('class',commonClassTwo);
+ commonDiv.appendChild(commonMainDiv);
+
+  commonImgclass = '';
+  commonImg.setAttribute('class',commonImgclass);
+  commonImg.setAttribute('src',imgsrc);
+  commonImg.setAttribute('alt',commonParagraph);
+commonMainDiv.appendChild(commonImg);
+  
+// d.innerHTML = `<a href=hreflink target="_blank" title=para>
+//            <div class=commonClassOne>
+//                 <div class=commonClassTwo>
+//                      <img src=imgsrc alt=data.para class=data.commonImgclass>
+//                           <div class=commonClassThree>    
+//                                <h5 class=commonHeader>heading</h5>
+//                                   <p class=commonParagraph>$(para)</p>
+//                         </div>
+//               </div>
+//          </div>
+//                 </a>`
      
     }   
 
 
 
+
+
+ //  let newELEMENT = (data) => {
+ //        return `<a href=${data.hreflink} target="_blank" title=${data.para}>
+ //          <div class=${data.commonClassOne}>
+ //               <div class=${data.commonClassTwo}>
+ //                    <img src=${data.imgsrc} alt=${data.para} class=${data.commonImgclass}>
+ //                         <div class=${data.commonClassThree}>    
+ //                              <h5 class=${data.commonHeader}>${data.heading}</h5>
+ //                                  <p class=${data.commonParagraph}>${data.para}</p>
+ //                         </div>
+ //               </div>
+ //          </div>
+ //                </a>`
+ // }
+
+// let data.commonImgclass , data.commonHeader , data.commonParagraph , data.commonClassOne , data.commonClassTwo , data.commonClassThree;
+//      data.commonImgclass = 'img-responsive';
+//      data.commonHeader = 'insight-title pb0';
+//      data.commonParagraph = 'insight-text';
+//      data.commonClassOne = 'col-eq-ht';
+//      data.commonClassTwo = 'bg-color1';
+//      data.commonClassThree = 'txt-reseach';
+  
 
 
 
