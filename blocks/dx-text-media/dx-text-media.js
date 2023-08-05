f0990417392aad967f00b7e3dc5c7e8a3f51b512
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-08-05 18:32:00 +0530

    Update dx-text-media.js

--- blocks/dx-text-media/dx-text-media.js
+++ blocks/dx-text-media/dx-text-media.js
@@ -91,11 +91,11 @@
     InsideRowSecondDivVideo.setAttribute('class','vidyard_iframe');
     InsideRowSecondDivVideo.setAttribute('title','Infosys Digital Experience');
     InsideRowSecondDivVideo.setAttribute('src',videoUrl);
-    // InsideRowSecondDivVideo.setAttribute('width','640');
-    // InsideRowSecondDivVideo.setAttribute('height','360');
-    // InsideRowSecondDivVideo.setAttribute('scrolling','no');
-    // InsideRowSecondDivVideo.setAttribute('frameborder','0');
-    // InsideRowSecondDivVideo.setAttribute('allowtransparency','true');
+    InsideRowSecondDivVideo.setAttribute('width','640');
+    InsideRowSecondDivVideo.setAttribute('height','360');
+    InsideRowSecondDivVideo.setAttribute('scrolling','no');
+    InsideRowSecondDivVideo.setAttribute('frameborder','0');
+    InsideRowSecondDivVideo.setAttribute('allowtransparency','true');
     // InsideRowSecondDivVideo.setAttribute('','allowfullscreen');
    
     InsideRowSecondDiv.appendChild(InsideRowSecondDivVideo);
