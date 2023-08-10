dd1df88bbb826f7f0d8ecb2c05eb21d06364c1e1
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-08-10 14:53:42 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -320,7 +320,7 @@ export default async function decorate(block) {
 	  insideDXOLLIAnchor.setAttribute('data-toggle', 'dropdown');
 	  insideDXOLLIAnchor.setAttribute('aria-expanded', 'false');
 	  insideDXOLLIDIV.appendChild(insideDXOLLIAnchor);
-	  insideDXOLLIAnchor.append(docContentDX + `<i class="fa fa-chevron-circle-down reverseArrow" aria-hidden="true"></i>`);
+	  insideDXOLLIAnchor.innerHTML = docContentDX;
 	  insideDXOLLIDIV.innerHTML = `
    						<ul class="dropdown-menu mega-dropdown-menu row" style="display: none;">
 								 
