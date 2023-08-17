355bfc6c2534b4f9ff30e1bb941b4df6184288bc
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-08-17 12:32:23 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -407,8 +407,9 @@ export default async function decorate(block) {
 	  insideDXImgElem.setAttribute('alt','Share');
 	  insideDXImgElem.setAttribute('class','share-icon');
 	  insideDXOLLIAnchorSecond.appendChild(insideDXImgElem);
-		
-	  var sampleULappend =   `
+
+	  insideDXOLSecondDIV.appendChild(insideDXOLLISecondDIV);
+	   var sampleULappend =   `
 		   	<ul class="social-share hidden-list list-inline" style="opacity: 1;">
 				<li>
 					<a href="javascript:void(0)" onclick="twitterShare(window.location.href, encodeURIComponent(document.title));" class="share" aria-label="ShareByTwitter"> 
@@ -443,8 +444,7 @@ export default async function decorate(block) {
 		
 			</ul>
 		 		  `;
-	  insideDXOLSecondDIV.appendChild(insideDXOLLISecondDIV);
-	  insideDXOLSecondDIV.appendChild(sampleULappend);
+	  insideDXOLSecondDIV.append(sampleULappend);
 	  
   }
 }
