be12c7aefd82a930c4e7d1329dd772fd7906789d
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-06 12:11:07 +0530

    Create sessionStorage.js

---
+++ scripts/sessionStorage.js
@@ -0,0 +1,12 @@
+var s = window.sessionStorage.email;
+console.log("sessionvalue:"+s);
+var digitalData = digitalData ||{};
+digitalData = {
+page:{
+pageInfo:{
+pageName:"Home Page";
+emailId:s; 
+console.log(emailId);
+}
+}
+};
