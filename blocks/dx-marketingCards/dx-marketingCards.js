40b53b5701e118f1588d31a50ecd15d542e4a50e
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-12 22:20:30 +0530

    Update dx-marketingCards.js

--- blocks/dx-marketingCards/dx-marketingCards.js
+++ blocks/dx-marketingCards/dx-marketingCards.js
@@ -1,1 +1,7 @@
-
+// getting main Div :document.querySelectorAll('.dx-marketingcards > div > div')[4].innerText;
+var mainDiv = document.querySelector('.dx-marketingcards-wrapper');
+//hiding main content
+document.querySelector('.dx-marketingcards-wrapper > .dx-marketingcards').style.display = 'none';
+// 
+var firstDiv = mainDiv.replace('dx-marketingcards-wrapper','containerV2');
+firstDiv.classList.add('container', 'responsivegrid', 'aem-GridColumn', 'aem-GridColumn--default--12');
