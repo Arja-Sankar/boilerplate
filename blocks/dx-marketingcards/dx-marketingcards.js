fc1f45756f9ba13414a96a472262d88c6b23d7d7
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-13 10:27:06 +0530

    Update dx-marketingcards.js

--- blocks/dx-marketingcards/dx-marketingcards.js
+++ blocks/dx-marketingcards/dx-marketingcards.js
@@ -12,8 +12,8 @@ var mainDiv = document.querySelector('.dx-marketingcards-wrapper');
 // var SixthContentLink = document.querySelectorAll('.dx-marketingcards > div > div')[5].innerText;
 
 // first div converting class name and adding class list
-var firstDiv = mainDiv.setAttribute('class','containerV2');
-firstDiv.classList.add('container', 'responsivegrid', 'aem-GridColumn', 'aem-GridColumn--default--12');
+mainDiv.setAttribute('class','containerV2');
+mainDiv.classList.add('container', 'responsivegrid', 'aem-GridColumn', 'aem-GridColumn--default--12');
 
 //getting content from Doc
 var firstContentCssLink = document.querySelectorAll('.containerV2 > div > div')[0].innerText;
@@ -21,7 +21,7 @@ var firstContentCssLink = document.querySelectorAll('.containerV2 > div > div')[
 //second Div adding to main div with class list
 var secondDiv = document.createElement('div');
 secondDiv.classList.add('cmp-containerV2', 'inset-py-relaxed', 'px-0', 'bg-transparent', 'bg-gradient-angled', 'from-transparent', 'to-transparent', 'cmp-containerV2-full-width', 'max-w-8xl', 'mx-auto', 'inline-cta--dark');
-firstDiv.appendChild(secondDiv);
+mainDiv.appendChild(secondDiv);
 
 //third Div adding to second div with class list
 var thirdDiv =  document.createElement('div');
