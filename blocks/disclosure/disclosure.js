5f314ce8fe95321aa9dd4db98df40ec30e86885f
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-21 15:40:25 +0530

    Update disclosure.js

--- blocks/disclosure/disclosure.js
+++ blocks/disclosure/disclosure.js
@@ -20,31 +20,31 @@ fourthDiv.classList.add('cg-global-disclosure-1', 'noindex');
   fourthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[0]);
   thirdDiv.appendChild(fourthDiv);
 }else if(i == 1){
-  var firstOne = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[1];
-  var secondTwo = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[2];
+  var firstOne = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[0];
+  var secondTwo = document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div > strong')[1];
   var fifthDiv = document.createElement('p');
   fifthDiv.classList.add('cg-global-disclosure-1', 'noindex');
-  fifthDiv.innerHTML = firstOne + secondTwo;
+  fifthDiv.appendChild(firstOne + secondTwo);
     thirdDiv.appendChild(fifthDiv);
 }else if(i == 2){
     var sixthDiv = document.createElement('p');
   sixthDiv.classList.add('cg-global-disclosure-1', 'noindex');
-  sixthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[2].innerText);
+  sixthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[0].innerText);
     thirdDiv.appendChild(sixthDiv);
 }else if(i == 3){
     var sevenDiv = document.createElement('p');
   sevenDiv.classList.add('cg-global-disclosure-1', 'noindex');
-  sevenDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[3].innerText);
+  sevenDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[0].innerText);
     thirdDiv.appendChild(sevenDiv);
 }else if(i == 4){
     var eigthDiv = document.createElement('p');
   eigthDiv.classList.add('cg-global-disclosure-1', 'noindex');
-  eigthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[4].innerText);
+  eigthDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[0].innerText);
     thirdDiv.appendChild(eigthDiv);
 }else if(i == 5){
     var ninethDiv = document.createElement('p');
   ninethDiv.classList.add('cg-global-disclosure-1', 'noindex');
-  ninethDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[5].innerText);
+  ninethDiv.appendChild(document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[0].innerText);
     thirdDiv.appendChild(ninethDiv);
 }
 }
