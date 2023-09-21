386f091944779c01a0c2524aa7b1e541b7079132
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-21 14:51:05 +0530

    Update disclosure.js

--- blocks/disclosure/disclosure.js
+++ blocks/disclosure/disclosure.js
@@ -1,1 +1,5 @@
-
+//document.querySelectorAll('.disclosure-wrapper > .disclosure.block > div > div')[0].innerText
+document.querySelector('.disclosure.block').style.display = 'none';
+//
+var divName = document.querySelector('.disclosure-wrapper');
+divName.classList.add('responsivegrid', 'cg-max-width', 'aem-GridColumn', 'aem-GridColumn--default--12');
