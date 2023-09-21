c09799079c415a7e79bdd1018b3c0517b7ebea3a
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2023-09-21 17:53:03 +0530

    Update cg-box-disclosure.js

--- blocks/cg-box-disclosure/cg-box-disclosure.js
+++ blocks/cg-box-disclosure/cg-box-disclosure.js
@@ -1,1 +1,11 @@
-//text cg-box-shadow-disclosure aem-GridColumn--tablet--10 aem-GridColumn--default--none aem-GridColumn--phone--none aem-GridColumn--phone--12 aem-GridColumn--tablet--none aem-GridColumn aem-GridColumn--default--8 aem-GridColumn--offset--default--2 aem-GridColumn--offset--phone--0 aem-GridColumn--offset--tablet--1
+//
+//hiding doc content
+document.querySelector('.cg-box-disclosure-wrapper > .cg-box-disclosure.block').style.display = 'none';
+var mainFirstDiv = document.querySelector('.cg-box-disclosure-wrapper');
+mainFirstDiv.classList.add('text', 'cg-box-shadow-disclosure', 'aem-GridColumn--tablet--10', 'aem-GridColumn--default--none', 'aem-GridColumn--phone--none', 'aem-GridColumn--phone--12', 'aem-GridColumn--tablet--none', 'aem-GridColumn', 'aem-GridColumn--default--8', 'aem-GridColumn--offset--default--2', 'aem-GridColumn--offset--phone--0', 'aem-GridColumn--offset--tablet--1');
+var secondDiv = document.createElement('div');
+secondDiv.setAttribute('class','cmp-text');
+mainFirstDiv.appendChild(secondDiv);
+var thirdDiv = document.createElement('p');
+thirdDiv.style.textalign = 'center';
+secondDiv.appendChild(thirdDiv);
